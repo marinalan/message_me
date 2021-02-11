6c8d37554b135a84cf88519256600c578a9148b2
Marina Landisberg <mlandisberg@gmail.com>
2021-02-10 23:27:57 -0800

    scrollable messages area, scrolling to bottom, hiding form submit button, sending messages on pressing Enter key

--- app/javascript/channels/chatroom_channel.js
+++ app/javascript/channels/chatroom_channel.js
@@ -1,5 +1,11 @@
 import consumer from "./consumer"
 
+var scroll_bottom = function() {
+  if ($('#messages').length >0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+};
+
 consumer.subscriptions.create("ChatroomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
@@ -13,5 +19,6 @@ consumer.subscriptions.create("ChatroomChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     $("#message-container").append(data["message"]);
+    scroll_bottom();
   }
 });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,9 +15,26 @@ Turbolinks.start()
 ActiveStorage.start()
 import "semantic-ui-sass";
 
+var scroll_bottom = function() {
+  if ($('#messages').length >0) {
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+};
+
+var submit_message = function() {
+  $('#message_body').on('keydown', function(e){
+    if (e.key == 'Enter') {
+      $('button').trigger('click');
+      e.target.value="";
+    }
+  });
+};
+
 document.addEventListener('turbolinks:load', () => {
   $('.ui.dropdown').dropdown();
   $('.message .close').on('click', function() {
       $(this).closest('.message').transition('fade');
   });
+  submit_message();
+  scroll_bottom();
 })
